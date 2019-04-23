6afcc9486c0fb772c8988b103842c47f1b8f0323
Nordine Saadouni <saadouni85@gmail.com>
2019-04-23 11:13:38 +0100

    changed all variale names to make them more appropriate. Completed the work for exchanging capabilities (i.e the object_hash_version) to decide which object_hash function to used based on both fssink and fssource

--- src/riak_repl2_fssink.erl
+++ src/riak_repl2_fssink.erl
@@ -256,7 +256,7 @@ decide_common_object_hash_version([], _TheirCaps) -> 0;
 decide_common_object_hash_version(_OurCaps, []) -> 0;
 decide_common_object_hash_version(OurCaps, TheirCaps) ->
     OurVersion = proplists:get_value(object_hash_version, OurCaps, 0),
-    TheirVersion = proplists:get_value(object_hash_version, TheirCaps, 0), 
+    TheirVersion = proplists:get_value(object_hash_version, TheirCaps, 0),
     lists:min([OurVersion, TheirVersion]).
 
 %% Depending on the protocol version number, send our capabilities

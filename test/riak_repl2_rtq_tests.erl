5ae89e040bd049fa3b35c8778728d42b5f065dbf
Nordine Saadouni <saadouni85@gmail.com>
2019-10-04 15:37:38 +0100

    fixed test

--- test/riak_repl2_rtq_tests.erl
+++ test/riak_repl2_rtq_tests.erl
@@ -43,7 +43,7 @@ rtq_trim_test() ->
 ask(Pid) ->
     Self = self(),
     gen_server:call(Pid, {pull_with_ack, rtq_test,
-             fun ({Seq, NumItem, Bin, _Meta, _AckList}) ->
+             fun ({Seq, NumItem, Bin, _Meta}) ->
                     Self ! {rtq_entry, {NumItem, Bin}},
                     gen_server:cast(Pid, {ack, rtq_test, Seq, os:timestamp()}),
                     ok
